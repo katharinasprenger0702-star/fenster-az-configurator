2ab32ef0ec32c330dd56927eae9faa47212e6bfe
katharinasprenger0702-star <katharina.sprenger.0702@gmail.com>
2025-09-05 22:18:46 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // Auto-generated export barrel for grouped price data
 
-export { default as fensterPrices } from "./groups/fenster.data.ts";
-export { default as balkontuerenPrices } from "./groups/balkontueren.data.ts";
-export { default as schiebetuerenPrices } from "./groups/schiebetueren.data.ts";
-export { default as haustuerenPrices } from "./groups/haustueren.data.ts";
-export { default as sonstigesPrices } from "./groups/sonstiges.data.ts";
+export { default as fensterPrices } from "./groups/fenster.data";
+export { default as balkontuerenPrices } from "./groups/balkontueren.data";
+export { default as schiebetuerenPrices } from "./groups/schiebetueren.data";
+export { default as haustuerenPrices } from "./groups/haustueren.data";
+export { default as sonstigesPrices } from "./groups/sonstiges.data";
